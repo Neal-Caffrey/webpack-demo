0d55fc3a9bd6d7859ba3a7084ad694b23d430cc0
刘克鹏 <kepengliu@huangbaoche.com>
2017-11-07 10:25:51 +0800

    commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// this is my first express page
+
 var express = require('express');
 var app = express();
 
--- src/static/index/index.js
+++ src/static/index/index.js
@@ -3,4 +3,3 @@ require('./scss/index.scss');
 var wrap = document.getElementById('wrap'),
 	html = '<span>good job!</span>';
 wrap.innerHTML = html;
-console.log('别提了')
